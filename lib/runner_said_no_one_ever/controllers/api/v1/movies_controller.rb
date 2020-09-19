468a45374fad75f317053973a0421f16624b9a28
Willian Antunes <willian.lima.antunes@gmail.com>
2020-09-19 20:54:34 -0300

    Implementation of /api/v1/movies route with its unit test

--- lib/runner_said_no_one_ever/controllers/api/v1/movies_controller.rb
+++ lib/runner_said_no_one_ever/controllers/api/v1/movies_controller.rb
@@ -2,9 +2,18 @@ module Controllers
   module API
     module V1
       class MoviesController < CustomSinatraBase
+        film_specialist = Business::FilmSpecialist
+
         namespace '/api/v1' do
           get '/movies' do
-            status 501
+            character, movie = film_specialist.yell_a_character_and_its_movie
+
+            # TODO: Get the value from ENVIRONMENT
+            # TODO: Allow the value to be customized through query string
+            sleep 1
+
+            content_type :json
+            { character: character, movie: movie }.to_json
           end
         end
       end
--- spec/unit/controllers/api/v1/movies_controller_spec.rb
+++ spec/unit/controllers/api/v1/movies_controller_spec.rb
@@ -3,10 +3,21 @@ require('spec_helper')
 RSpec.describe(Controllers::API::V1::MoviesController) do
   let(:request_path) { '/api/v1/movies' }
 
-  context 'When asked details about a movie' do
-    it 'should receive 501 as it is not implemented yet' do
+  context 'When asked details about the topic' do
+    it 'should return a random character and its movie' do
+      controller_class = Controllers::API::V1::MoviesController
+
+      allow_any_instance_of(controller_class).to(receive(:sleep))
+      expect_any_instance_of(controller_class).to(receive(:sleep).with(1))
+      fake_value = %w[Jafar Aladdin]
+      expect(Business::FilmSpecialist).to(receive(:yell_a_character_and_its_movie).and_return(fake_value))
+
       get request_path
-      expect(last_response.status).to(be(501))
+
+      expected_body = { "character": fake_value.first, "movie": fake_value.last }
+      expect(last_response.status).to(be(200))
+      body_as_json = JSON.parse(last_response.body, { symbolize_names: true })
+      expect(body_as_json).to(include(**expected_body))
     end
   end
 end
